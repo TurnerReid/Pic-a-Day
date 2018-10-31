3d65c88f029aeaf211bb037331e7998362a6e3da
Clifford Fung <fung.clifford@gmail.com>
2018-10-30 19:36:02 -0700

    added folder

---
+++ VisualCalendar/CalendarVars.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarVars.swift
+//  VisualCalendar
+//
+//  Created by Clifford Fung on 2018-10-30.
+//  Copyright © 2018 Clifford Fung. All rights reserved.
+//
+
+import Foundation
+
+
+let date = Date()
+let calendar = Calendar.current
+let day = calendar.component(.day , from: date)
+var weekday = calendar.component(.weekday, from: date) - 1
+var month = calendar.component(.month, from: date) - 1
+var year = calendar.component(.year, from: date)
